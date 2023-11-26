0e0f7513593d22d5ec26f2289124e44a1e6cdfdf
AlphaRora <adoba@alpharora.com>
2023-11-25 23:48:01 -0500

    simple

--- components/ui/CenteredCTAText/index.jsx
+++ components/ui/CenteredCTAText/index.jsx
@@ -6,7 +6,7 @@ const CenteredCTAText = () => {
             <div className="custom-screen text-center">
                 <div className="max-w-xl mx-auto">
                     <h2 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
-                        We help you secure your most critical workloads
+                        We help secure your most critical workloads
                     </h2>
                     <p className="mt-3 text-gray-600">
                         Panpatico provides you with complete control on access, activity and usage of your cloud applications, workloads and resources, for whoever and whenever
